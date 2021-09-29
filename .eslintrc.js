0fe30f29b411172607c951506147636b6b2842c0
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-09-29 23:47:46 +0900

    vuetify

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+  extends: ['plugin:vue/recommended'],
+  plugins: [
+    'vuetify'
+  ],
+  rules: {
+    'vuetify/no-deprecated-classes': 'error'
+  }
+}
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
@@ -16,5 +17,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
